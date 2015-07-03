6ed7d81827c794c071b99508e3999c454629c9e5
Dan Barua <danbarua@gmail.com>
2015-07-03 11:49:07 +0100

    Drink every time someone drops the m-bomb

---
+++ README.md
@@ -0,0 +1,12 @@
+mongolian-clusterfucks
+=============
+
+Chrome extension that replaces occurrences of 'micro services' with 'mongolian clusterfuck'
+
+[Direct download of crx file](https://github.com/danbarua/mongolian-clusterfucks/blob/master/MongolianClusterFucks.crx?raw=true)
+
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag MongolianClusterfucks.crx into the page that appears.
---
+++ Source/content_script.js
@@ -0,0 +1,48 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bMicroServices\b/g, "MongolianClusterfucks");
+	v = v.replace(/\bMicro Services\b/g, "Mongolian Clusterfucks");
+	v = v.replace(/\bMicro services\b/g, "Mongolian clusterfucks");
+	v = v.replace(/\micro Services\b/g, "mongolian Clusterfucks");
+	v = v.replace(/\bmicro services\b/g, "mongolian clusterfucks");
+	v = v.replace(/\bmicroservices\b/g, "mongolianclusterfucks");
+	v = v.replace(/\bMicroservices\b/g, "Mongolianclusterfucks");
+	v = v.replace(/\bMicroservice\b/g, "Mongolianclusterfuck");
+	v = v.replace(/\bmicroservice\b/g, "mongolianclusterfuck");
+	v = v.replace(/\bmicroService\b/g, "mongolianClusterfuck");
+	
+	textNode.nodeValue = v;
+}
+
+
